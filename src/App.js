60fde5ebfe6a6f1197690498ee041d0f44a2a642
Danny Jaramillo <dajaramillogu@uide.edu.ec>
2023-07-05 20:09:02 -0500

    1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button, Input, MenuList, MenuItem, ListItemText, ListItemIcon, InputAdornment} from '@mui/material';
+import InicioIcon from '@mui/icons-material/Home';
+import TareasIcon from '@mui/icons-material/Book';
+import EmailIcon from '@mui/icons-material/Email';
+import BusquedaIcon from '@mui/icons-material/Search';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+        <MenuList>
+          <MenuItem> 
+            <ListItemIcon> <InicioIcon></InicioIcon> </ListItemIcon>
+            <ListItemText> Inicio </ListItemText>
+          </MenuItem>
+          <MenuItem> 
+            <ListItemIcon> <TareasIcon></TareasIcon> </ListItemIcon>
+            <ListItemText> Tareas </ListItemText>
+          </MenuItem>
+          <MenuItem> 
+            <ListItemIcon> <EmailIcon></EmailIcon> </ListItemIcon>
+            <ListItemText> Correo </ListItemText>
+          </MenuItem>
+        </MenuList>
+      </div>
+      <div>
+        <Input id = "input-wich-icon-adornment"
+        startAdornment ={
+        <InputAdornment position="start">
+          <BusquedaIcon></BusquedaIcon>
+        </InputAdornment>
+        }
+        />
+
+      </div>
       </header>
     </div>
+    
   );
 }
 
